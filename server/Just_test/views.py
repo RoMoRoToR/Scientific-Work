2b8013bb3e0b7db2d2255b39292c665e1f881303
Appjey <loikoanton@yandex.ru>
2023-03-10 21:47:02 +0300

    Update BackEnd Faster, Better, Stronger

--- server/Just_test/views.py
+++ server/Just_test/views.py
@@ -1,6 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
-
+from .models import Question
 
 def index(request):
-    return HttpResponse("Hi Rodion and Tosha!")
+
+    context = {
+        'num_books': 1,
+        'num_instances': 2,
+        'num_instances_available': 3,
+        'num_authors': 4,
+    }
+
+    return render(request, 'index.html', context=context)
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'detail.html', {'question': question})
